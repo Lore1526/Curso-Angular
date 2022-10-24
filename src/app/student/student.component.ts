afefe8d1e10097c73f4bcd79f84df08b5065cc3c
Lorena De Luca <lore.1526@hotmail.com>
2022-10-23 22:10:23 -0300

    destroy

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -4,43 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from './student';
 import { MatDialog } from '@angular/material/dialog';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  edit: string;
-  delete: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', edit: '', delete: '' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', edit: '', delete: '' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', edit: '', delete: '' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', edit: '', delete: '' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', edit: '', delete: '' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', edit: '', delete: '' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', edit: '', delete: '' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', edit: '', delete: '' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', edit: '', delete: '' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', edit: '', delete: '' },
-
-];
-
-const students: Student[] = [
-  { Id: 1, Name: 'Juan', Surname: 'Perez', Course: 'A', HourCourse: 'Mañana' },
-  { Id: 2, Name: 'Pedro', Surname: 'Garcia', Course: 'A', HourCourse: 'Noche' },
-  { Id: 3, Name: 'Carlos', Surname: 'Lopez', Course: 'C', HourCourse: 'Tarde' },
-  { Id: 4, Name: 'Jorge', Surname: 'Gutierrez', Course: 'B', HourCourse: 'Tarde' },
-  { Id: 5, Name: 'Luis', Surname: 'Dominguez', Course: 'B', HourCourse: 'Mañana' },
-  { Id: 6, Name: 'Sandro', Surname: 'Rodriguez', Course: 'B', HourCourse: 'Mañana' },
-  { Id: 7, Name: 'Jorge', Surname: 'Campanella', Course: 'C', HourCourse: 'Noche' },
-  { Id: 8, Name: 'María', Surname: 'Sanchez', Course: 'B', HourCourse: 'Tarde' },
-  { Id: 9, Name: 'Juana', Surname: 'Liberal', Course: 'C', HourCourse: 'Noche' },
-  { Id: 10, Name: 'Margarita', Surname: 'Olivieri', Course: 'A', HourCourse: 'Tarde' },
-]
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-student',
@@ -48,19 +12,22 @@ const students: Student[] = [
   styleUrls: ['./student.component.css']
 })
 
-
 export class StudentComponent implements OnInit {
 
   displayedColumns: string[] = ['Id', 'Name', 'Course', 'HourCourse', 'edit', 'delete'];
   dataSource: Student[] = [];
-
+  studentsSubscription!: Subscription;
 
   constructor(private router: Router, private studentService: StudentServiceService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.studentService.getStudent().subscribe(
+    this.studentsSubscription = this.studentService.getStudent().subscribe(
       (students) => this.dataSource = students
     );
+  }
+
+  ngOnDestroy(): void {
+    this.studentsSubscription.unsubscribe();
   }
 
   navigateToAddEditStudent(): void {
