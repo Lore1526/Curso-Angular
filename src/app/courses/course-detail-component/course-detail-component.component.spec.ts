24750c00f013683f3a915fceeae8ef4519b23776
Lorena De Luca <lore.1526@hotmail.com>
2022-10-27 19:45:54 -0300

    box

--- src/app/courses/course-detail-component/course-detail-component.component.spec.ts
+++ src/app/courses/course-detail-component/course-detail-component.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CourseDetailComponentComponent } from './course-detail-component.component';
 
 describe('CourseDetailComponentComponent', () => {
--- src/app/courses/detail-course/detail-course.component.spec.ts
+++ src/app/courses/detail-course/detail-course.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DetailCourseComponent } from './detail-course.component';
 
 describe('DetailCourseComponent', () => {
