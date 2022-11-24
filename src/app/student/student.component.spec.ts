50f3e93e261cfbc070799bee54030da424107321
Lucas Cerbino <Lucas.Cerbino@emerios.com>
2022-11-23 22:41:26 -0300

    add tests

--- src/app/student/student.component.spec.ts
+++ src/app/student/student.component.spec.ts
@@ -6,6 +6,9 @@ import { StudentComponent } from './student.component';
 import { Router } from '@angular/router';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Student } from './student';
+import { Observable, of } from 'rxjs';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DeleteStudentComponent } from './delete-student/delete-student.component';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
@@ -37,10 +40,23 @@ describe('StudentComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('ngOnit: should fill datasource', async () => {
+    const students :Student[] = [{
+        Id : 1,
+        Name: 'Lorena',
+        Surname: 'De Luca',
+        HourCourse: 'morning',
+        Course: 'Angular'
+      }];
+    spyOn(studentService,'getStudent').and.returnValues(of(students));
+
+    component.ngOnInit();
+    expect(component.dataSource).toBe(students);
+  });
+
   it('navigateToAddEditStudent:should navigate to addEditStudent', () =>{
     const component = fixture.componentInstance;
     const navigateSpy = spyOn(router, 'navigate');
-
     component.navigateToAddEditStudent();
     expect(navigateSpy).toHaveBeenCalledWith(['/addEditStudent']);
   })
@@ -49,8 +65,33 @@ describe('StudentComponent', () => {
     const component = fixture.componentInstance;
     const navigateSpy = spyOn(router, 'navigate');
     let student : any;
-
     component.editStudent(student);
     expect(navigateSpy).toHaveBeenCalledWith(['/addEditStudent',student]);
   })
+
+  it('delete Student: should open Delete component', () =>{
+    const students :Student[] = [{
+        Id : 1,
+        Name: 'Lorena',
+        Surname: 'De Luca',
+        HourCourse: 'morning',
+        Course: 'Angular'
+      }];
+    const component = fixture.componentInstance;
+    const dialogOpen = spyOn(component.dialog, 'open')
+    .and
+    .returnValue({
+        afterClosed: () => of("Yes")
+    } as MatDialogRef<typeof component>);
+    const serviceSpy = spyOn(studentService, 'deleteStudent').and.returnValue(of());
+    
+    component.deleteStudent(students[0]);
+    expect(dialogOpen).toHaveBeenCalledWith(DeleteStudentComponent, 
+        {
+            width: '250px'
+        });
+
+    expect(serviceSpy).toHaveBeenCalledWith(students[0].Id);
+  })
+
 });
